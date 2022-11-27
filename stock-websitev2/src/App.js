ebe88086e8d0f9c7be95d09b941006e1cc8a76e3
Ghana Ahmada Yudistira <103763579+ghanahmada@users.noreply.github.com>
2022-11-27 17:11:08 +0700

    Update App.js

--- stock-websitev2/src/App.js
+++ stock-websitev2/src/App.js
@@ -1,12 +1,22 @@
-import './App.css';
-import LandingPage from './components/LandingPage/LandingPage';
+// import './App.css';
+// import LandingPage from './components/LandingPage/LandingPage';
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>pls</h1>
+//       <LandingPage></LandingPage>
+//     </div>
+//   );
+// }
+
+// export default App;
+import "./App.css";
+import Tabs from "./components/TabComponent/Tabs";
 function App() {
   return (
     <div className="App">
-      <h1>pls</h1>
-      <LandingPage></LandingPage>
+      <Tabs />
     </div>
   );
 }
-
 export default App;
