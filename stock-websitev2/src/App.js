91b1e134f3c69f6260fa45cd80247ef75d2e106e
Oey Joshua <102236084+Metry630@users.noreply.github.com>
2022-11-28 20:54:15 +0700

    restore parts that were erased in merge

--- stock-websitev2/src/App.js
+++ stock-websitev2/src/App.js
@@ -1,4 +1,17 @@
+// import './App.css';
+// import LandingPage from './components/LandingPage/LandingPage';
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>pls</h1>
+//       <LandingPage></LandingPage>
+//     </div>
+//   );
+// }
 
+// export default App;
+import "./App.css";
+import Tabs from "./components/TabComponent/Tabs";
 function App() {
   return (
     <div className="App">
@@ -6,4 +19,5 @@ function App() {
     </div>
   );
 }
+
 export default App;
