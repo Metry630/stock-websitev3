8e35ab241349ba2dded009e9496ed4d8212c3ce5
Ghana Ahmada Yudistira <103763579+ghanahmada@users.noreply.github.com>
2022-11-30 23:22:48 +0700

    Update index.js

--- stock-websitev2/src/index.js
+++ stock-websitev2/src/index.js
@@ -1,15 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  Outlet,
+} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import Home2 from "./routes/Logo";
+import Home from "./routes/Home";
+import News from "./routes/News";
+import LearningResources from "./routes/LearningResources/LearningResources";
+import Graphs from "./routes/Graphs";
+import Signup from "./routes/Signup";
+import ErrorPage from "./routes/ErrorPage";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const AppLayout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "news",
+        element: <News />,
+      },
+      {
+        path: "learning",
+        element: <LearningResources />,
+      },
+      {
+        path: "graphs",
+        element: <Graphs />,
+      },
+      {
+        path: "signup",
+        element: <Signup />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
