f3a37ec997d32fb0e8b5303f075297d9c256abb1
Ghana Ahmada Yudistira <103763579+ghanahmada@users.noreply.github.com>
2022-12-03 02:30:41 +0700

    Update ResData.js

--- stock-websitev2/src/routes/LearningResources/ResData.js
+++ stock-websitev2/src/routes/LearningResources/ResData.js
@@ -19,59 +19,34 @@ import advanced_7 from "../../images/LearningResources/advanced_7.jpg"
 export const Beginner = [
     {
       id: 1,
-      title: 'Mario Kart™ 8 Deluxe',
-      price: '$59.99',
-      category: 'Nintendo Switch',
       linkImg: beginner_1,
     },
     {
       id: 2,
-      title: 'TRIANGLE STRATEGY™',
-      price: '$59.99',
-      category: 'Nintendo Switch',
       linkImg: beginner_2,
     },
     {
       id: 3,
-      title: 'Pokémon™ Legends: Arceus',
-      price: '$59.99',
-      category: 'Nintendo Switch',
       linkImg: beginner_3,
     },
     {
       id: 4,
-      title: 'Super Mario™ 3D World + Bowser’s Fury',
-      price: '$59.99',
-      category: 'Nintendo Switch',
       linkImg:beginner_4,
     },
     {
       id: 5,
-      title: 'Cuphead',
-      price: '$19.99',
-      category: 'Nintendo Switch',
       linkImg: beginner_5,
     },
     {
       id: 6,
-      title: 'Minecraft',
-      price: '$29.99',
-      category: 'Nintendo Switch',
       linkImg: beginner_6,
     },
     {
       id: 7,
-      title: 'Mario + Rabbids® Kingdom Battle',
-      price: '$59.99',
-      category: 'Nintendo Switch',
       linkImg: beginner_7,
     },
     {
       id: 8,
-      title: 'Unravel Two',
-      price: '$59.99',
-      category: 'Nintendo Switch',
-      sale: 63, //percent
       linkImg: beginner_8,
     },
   ];
@@ -79,51 +54,30 @@ export const Beginner = [
   export const Advanced = [
     {
       id: 1,
-      title: 'Mario Kart™ 8 Deluxe',
-      price: '$59.99',
-      category: 'Nintendo Switch',
       linkImg: advanced_1,
     },
     {
       id: 2,
-      title: 'TRIANGLE STRATEGY™',
-      price: '$59.99',
-      category: 'Nintendo Switch',
       linkImg: advanced_2,
     },
     {
       id: 3,
-      title: 'Pokémon™ Legends: Arceus',
-      price: '$59.99',
-      category: 'Nintendo Switch',
       linkImg: advanced_3,
     },
     {
       id: 4,
-      title: 'Super Mario™ 3D World + Bowser’s Fury',
-      price: '$59.99',
-      category: 'Nintendo Switch',
       linkImg: advanced_4,
     },
     {
       id: 5,
-      title: 'Cuphead',
-      price: '$19.99',
-      category: 'Nintendo Switch',
       linkImg: advanced_5,
     },
     {
       id: 6,
-      title: 'Minecraft',
-      price: '$29.99',
-      category: 'Nintendo Switch',
       linkImg: advanced_6,
     },
     {
       id: 7,
-      title: 'Mario + Rabbids® Kingdom Battle',
-      price: '$59.99',
-      category: 'Nintendo Switch',
       linkImg: advanced_7,
     },
   ];
